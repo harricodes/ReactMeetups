4687696d82f302a946bd7375f4dd4353a0c3d673
Harrison Njeru <harrisonnjeru7@gmail.com>
2021-10-26 16:18:22 +0300

    Data Showing

--- src/pages/AllMeetups.js
+++ src/pages/AllMeetups.js
@@ -12,8 +12,18 @@ function AllMeetups(){
             return response.json();
           })
           .then((data) => {
+              const meetups = [];
+
+              for (const key in data){
+                  const meetup = {
+                      id: key,
+                      ...data[key]
+                  };
+
+                  meetups.push(meetup);
+              }
             setIsLoading(false);
-            setloadedMeetups(data);
+            setloadedMeetups(meetups);
           });
     }, [])
     
